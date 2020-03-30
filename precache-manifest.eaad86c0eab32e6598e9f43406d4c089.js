421b703c4c4b32511d1ac6456980c27f14772bb3
Selvasankar Perumal <selvainnovations@gmail.com>
2020-03-30 13:53:46 +0000

    Updates

---
+++ precache-manifest.eaad86c0eab32e6598e9f43406d4c089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218e18dfaa1698c940187115e741f2ad",
+    "url": "/my-counter-app/index.html"
+  },
+  {
+    "revision": "cff2e55d8b25c0df1192",
+    "url": "/my-counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca48da8ea057f11c29d0",
+    "url": "/my-counter-app/static/js/2.654e3526.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-counter-app/static/js/2.654e3526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff2e55d8b25c0df1192",
+    "url": "/my-counter-app/static/js/main.4b7095ca.chunk.js"
+  },
+  {
+    "revision": "1e781d4f92bf05bf1da2",
+    "url": "/my-counter-app/static/js/runtime-main.23aa387f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-counter-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
